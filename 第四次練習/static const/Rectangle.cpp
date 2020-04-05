db9fb64659de4be8bff55b235a53095b6cd511df
hoopizs1452 <45253893+hoopizs1452@users.noreply.github.com>
2020-04-05 12:41:53 +0800

    Update Rectangle.cpp

--- 第四次練習/static const/Rectangle.cpp
+++ 第四次練習/static const/Rectangle.cpp
@@ -9,7 +9,6 @@ Rectangle::Rectangle()
     numOfRect++;
 }
 
-// Construct a circle object
 Rectangle::Rectangle(double newWidth, double newHeight)
 {
     width = newWidth;
@@ -17,13 +16,11 @@ Rectangle::Rectangle(double newWidth, double newHeight)
     numOfRect++;
 }
 
-// Return the area of this circle
 double Rectangle::getArea() const
 {
     return width * height;
 }
 
-// Return the radius of this circle
 double Rectangle::getWidth() const
 {
     return width;
@@ -34,7 +31,6 @@ double Rectangle::getHeight() const
     return height;
 }
 
-// Set a new radius
 void Rectangle::setWidth(double newWi)
 {
     width = (newWi >= 0) ? newWi : 0;
@@ -45,7 +41,6 @@ void Rectangle::setHeight(double newHe)
     height = (newHe >= 0) ? newHe : 0;
 }
 
-// Return the number of circle objects
 int Rectangle::geNumOfRect()
 {
     return numOfRect;
