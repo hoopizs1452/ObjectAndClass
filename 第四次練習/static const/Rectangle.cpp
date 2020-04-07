8b1f69a6a3488c57f0f207166c891d57c34fc61c
hoopizs1452 <45253893+hoopizs1452@users.noreply.github.com>
2020-04-07 15:10:18 +0800

    Update Rectangle.cpp

--- 第四次練習/static const/Rectangle.cpp
+++ 第四次練習/static const/Rectangle.cpp
@@ -4,44 +4,44 @@ int Rectangle::numOfRect = 0;
 
 Rectangle::Rectangle()
 {
-    width = 1;
-    height = 1;
-    numOfRect++;
+	width = 1;
+	height = 1;
+	numOfRect++;
 }
 
 Rectangle::Rectangle(double newWidth, double newHeight)
 {
-    width = newWidth;
-    height = newHeight;
-    numOfRect++;
-}
-
-double Rectangle::getArea() const
-{
-    return width * height;
-}
-
-double Rectangle::getWidth() const
-{
-    return width;
-}
-
-double Rectangle::getHeight() const
-{
-    return height;
+	width = newWidth;
+	height = newHeight;
+	numOfRect++;
 }
 
 void Rectangle::setWidth(double newWi)
 {
-    width = (newWi >= 0) ? newWi : 0;
+	width = newWi;
 }
 
 void Rectangle::setHeight(double newHe)
 {
-    height = (newHe >= 0) ? newHe : 0;
+	height = newHe;
 }
 
-int Rectangle::geNumOfRect()
+double Rectangle::getWidth() const
 {
-    return numOfRect;
+	return width;
+}
+
+double Rectangle::getHeight() const
+{
+	return height;
+}
+
+int Rectangle::getNumOfRect()
+{
+	return numOfRect;
+}
+
+double Rectangle::getArea() const
+{
+	return width * height;
 }
