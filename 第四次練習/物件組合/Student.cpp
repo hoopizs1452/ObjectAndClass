93aec0e40988e7e1350917378f02ff981e1391e0
hoopizs1452 <45253893+hoopizs1452@users.noreply.github.com>
2020-04-07 14:31:15 +0800

    Update Student.cpp

--- 第四次練習/物件組合/Student.cpp
+++ 第四次練習/物件組合/Student.cpp
@@ -4,26 +4,26 @@
 #include "Student.h"
 using namespace std;
 
-Student::Student(string newName, Date newDate, int newScore)
+Student::Student(string n, Date b, int s)
+{
+	name = n;
+	birthDay = b;
+	score = s;
+}
+
+void Student::setName(string newName)
 {
 	name = newName;
-	birthDay.setDate(newDate.getMonth(), newDate.getDay(), newDate.getYear());
+}
+
+void Student::setDate(Date newDate)
+{
+	birthDay = newDate;
+}
+
+void Student::setScore(int newScore)
+{
 	score = newScore;
-}
-
-void Student::setName(string newN)
-{
-	name = newN;
-}
-
-void Student::setDate(Date newD)
-{
-	birthDay.setDate(newD.getMonth(), newD.getDay(), newD.getYear());
-}
-
-void Student::setScore(int newS)
-{
-	score = newS;
 }
 
 string Student::getName() const
@@ -44,6 +44,6 @@ int Student::getScore() const
 void Student::print()
 {
 	cout << name << " ";
-	cout << birthDay.getMonth() << "/" << birthDay.getDay() << "/" << birthDay.getYear() << " ";
+	birthDay.print();
 	cout << score << endl;
 }
