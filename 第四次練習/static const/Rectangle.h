5d96a6e43519725b1c0d85b9a0e9f7d1aa163faa
hoopizs1452 <45253893+hoopizs1452@users.noreply.github.com>
2020-04-07 15:09:54 +0800

    Update Rectangle.h

--- 第四次練習/static const/Rectangle.h
+++ 第四次練習/static const/Rectangle.h
@@ -1,22 +1,22 @@
-//#pragma once
 #ifndef Rectangle_H
 #define Rectangle_H
 
 class Rectangle
 {
 public:
-    Rectangle();
-    Rectangle(double, double);
-    double getArea() const;
-    double getWidth() const;
-    double getHeight() const;
-    void setWidth(double);
-    void setHeight(double);
-    static int geNumOfRect();
+	Rectangle();
+	Rectangle(double, double);
+	void setWidth(double);
+	void setHeight(double);
+	double getWidth() const;
+	double getHeight() const;
+	double getArea() const;
+	
+	static int getNumOfRect();
 
 private:
-    double width, height;
-    static int numOfRect;
+	double width, height;
+	static int numOfRect;
 };
 
 #endif
