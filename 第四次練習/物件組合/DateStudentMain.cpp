ccce73dd3d37ef8689fabc309c1e0b3881eeca06
hoopizs1452 <45253893+hoopizs1452@users.noreply.github.com>
2020-04-07 14:29:51 +0800

    Update DateStudentMain.cpp

--- 第四次練習/物件組合/DateStudentMain.cpp
+++ 第四次練習/物件組合/DateStudentMain.cpp
@@ -7,18 +7,17 @@ using namespace std;
 int main()
 {
 	string name;
-	int year, month, day;
-	Date birth1;
-	birth1.setDate(6, 1, 1999);
-	Date birth2;
-	birth2.setDate(10, 8, 1997);
+	int month, day, year, score;
+	Date birth1(6, 1, 1999);
+	Date birth2(10, 8, 1997);
 	Student student1("John", birth1, 90);
 	Student student2("Marry", birth2, 80);
+
 	cin >> name >> month >> day >> year;
-	
+
+	Date birth3(month, day, year);
 	student1.setName(name);
-	birth2.setDate(month, day, year);
-	student2.setDate(birth2);
+	student2.setDate(birth3);
 
 	student1.print();
 	student2.print();
