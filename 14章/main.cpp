62e993f22645d91605cc3a61504c06f4f3b2b2ae
hoopizs1452 <45253893+hoopizs1452@users.noreply.github.com>
2020-05-19 00:04:43 +0800

    update

--- 14章/main.cpp
+++ 14章/main.cpp
@@ -10,17 +10,14 @@ int main()
 	Rational r4(7, 2);
 	Rational r5(5, 6);
 
-	cout << "first=" << r1 + r2 * r3 << endl;
-	cout << "second=" << (r4 - r5) / (r2) << endl;
-	cout << "++first=" << ++(r1 + r2 * r3) << endl;
-	cout << "--second=" << --((r4 - r5) / (r2)) << endl;
-	if (++(r1 + r2 * r3) > --((r4 - r5) / (r2)))
-	{
-		cout << ++(r1 + r2 * r3) << ">" << --((r4 - r5) / (r2)) << endl;
-	}
-	else
-		cout << ++(r1 + r2 * r3) << "<" << --((r4 - r5) / (r2)) << endl;
+	Rational first = r1 + (r2 * r3);
+	Rational second = (r4 - r5) / r2;
+
+	cout << "first=" << first << endl;
+	cout << "second=" << second << endl;
+	cout << "++first=" << ++first << endl;
+	cout << "--second=" << --second << endl;
+	cout << first << ((first > second) ? ">" : ((first < second) ? "<" : "=")) << second << endl;
 	
-	system("pause");
 	return 0;
 }
